97d822ab655118549c5a594612499a942e9fe249
Sylvain Mathieu <sylvain.mathieu@gmail.com>
2013-09-17 15:59:48 +0100

    Disable browser cache in dev mode

--- app/controllers/stylus/StylusController.java
+++ app/controllers/stylus/StylusController.java
@@ -1,5 +1,6 @@
 package controllers.stylus;
 
+import play.Play;
 import play.modules.stylus.StylusCompiler;
 import play.mvc.Controller;
 
@@ -9,7 +10,9 @@ public class StylusController extends Controller {
 		String compiledStyle = StylusCompiler.compileStyle(styleName);
 		response.setHeader("Content-Length", Integer.toString(compiledStyle.length()));
 		response.contentType = "text/css";
-		response.cacheFor("1h");
+		if (Play.mode.isProd()) {
+			response.cacheFor("1h");
+		}
 		renderText(compiledStyle);
 	}
 
